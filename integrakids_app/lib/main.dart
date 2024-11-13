2819d8856f423e0a9c575e8854219703031a0f9f
Rodrigo Thenopholo <thenopholo92@icloud.com>
2024-11-13 15:40:50 -0300

    Firebase integrado

--- integrakids_app/lib/main.dart
+++ integrakids_app/lib/main.dart
@@ -1,11 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
+import 'firebase_options.dart';
 import 'src/integrakids_app.dart';
 
 Future<void> main() async {
   await initializeDateFormatting();
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    if (Firebase.apps.isEmpty) {
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+    }
+  } catch (e) {
+    print('Erro ao inicializar o Firebase: $e');
+  }
   runApp(
     const ProviderScope(
       child: IntegrakidsApp(),
